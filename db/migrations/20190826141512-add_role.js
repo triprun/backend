52348f2a748d8e5b460a9b9038e84464034d924c
Nikita <xnekitosx@list.ru>
2019-08-26 17:18:00 +0300

    add migrate

---
+++ db/migrations/20190826141512-add_role.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+        'Users',
+        'role',
+        {
+          type: Sequelize.INTEGER,
+          defaultValue: 0
+        }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
